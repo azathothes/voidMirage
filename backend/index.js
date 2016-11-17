18b4ea304f4bf09a0d308773a89200a3856ca0c1
wanlf <azathothes@outlook.com>
2016-11-17 18:04:40 +0800

    add 404 handler

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,7 @@ const router = require('./routes/index_routes.js');
 const bodyparser = require('koa-body-parser');
 const session = require('koa-session');
 app.keys = ['wanlf\'s little srcret'];
+
 app.use(bodyparser());
 app.use(serve(__dirname+'/Public'));
 app.use(views(__dirname + '/views',{
@@ -16,4 +17,8 @@ app.use(views(__dirname + '/views',{
 }))
 app.use(session({maxAge:3*60*1000,key:'test-session-key'},app));
 app.use(router.routes());
+app.use(function* (){
+	this.response.status = 404;
+	yield this.render('404not_found');
+});
 app.listen(3000);
